bba26ada2bb4511ae0513119a67299ae6c2a03e4
Lucas Carmohn <>
2021-05-18 12:53:54 +0200

    initial commit

---
+++ src/main/JulianDate.java
@@ -0,0 +1,29 @@
+package main;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ */
+public class JulianDate {
+
+    //Class variables
+    public static final LocalDateTime START_DATE = LocalDateTime.of(-4712, 1, 1, 12,0, 0, 0);
+
+
+    public static void main (String args []) {
+
+        System.out.println(START_DATE);
+
+    }
+
+
+
+
+
+    //Fields
+
+    //Constructors
+    public JulianDate() {
+    }
+}
