01eb01728782531cfb4ba13a5aad33c6aaf5bf93
Lucas Carmohn <>
2021-05-21 14:39:52 +0200

    Implement exercise 3 and add several tests for the MetricDate

--- result/test/MetricDateTest.java
+++ result/test/MetricDateTest.java
@@ -1,27 +1,35 @@
 package result.test;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.Assert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import result.main.JulianDate;
-
-import java.util.Optional;
-
-import static org.junit.Assert.assertEquals;
+import result.main.MetricDate;
 
 class MetricDateTest {
 
-    private JulianDate julianDate;
+    MetricDate md = new MetricDate();
 
-    @BeforeEach
-    public void setUp() throws Exception {
-        julianDate = new JulianDate(1582, 10, 15);
+    @Test
+    @DisplayName("Test if converting Julian date to Metric date works with a small number")
+    void ConvertJulianToMetricDateWithSmallNumber() {
+        Assert.assertEquals("Converting Julian date to Metric date.", "1.6.1.2299", md.julianToMetricDate(2299160.5));
     }
 
     @Test
-    @DisplayName("Test if the tomorrow method works correctly")
-    void testTomorrow() {
-        //TODO Test erstellen
+    @DisplayName("Test if converting Julian date to Metric date works with a large number")
+    void ConvertJulianToMetricDateWithLargeNumber() {
+        Assert.assertEquals("Converting Julian date to Metric date.", "1.2.8.993468", md.julianToMetricDate(993468829));
     }
 
+    @Test
+    @DisplayName("Test if converting Metric date to Julian date works with a small number")
+    void ConvertMetricToJulianDateWithSmallNumber() {
+        Assert.assertEquals("Converting Metric date to Julian date.", 2299161, md.metricToJulianDate(1, 6 , 1, 2299));
+    }
+
+    @Test
+    @DisplayName("Test if converting Metric date to Julian date works with a large number")
+    void ConvertMetricToJulianDateWithLargeNumber() {
+        Assert.assertEquals("Converting Metric date to Julian date.", 10000500, md.metricToJulianDate(100, 50 , 9, 9999));
+    }
 }
